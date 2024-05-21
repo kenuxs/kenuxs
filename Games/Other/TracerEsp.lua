213de8afc7bf4dd28bdd99886fd9005ecaac9ceb
kenuxs <144792959+kenuxs@users.noreply.github.com>
2024-05-21 20:59:24 +0200

    Create TracerEsp.lua

---
+++ Games/Other/TracerEsp.lua
@@ -0,0 +1,143 @@
+
+local function API_Check()
+    if Drawing == nil then
+        return "No"
+    else
+        return "Yes"
+    end
+end
+
+local Find_Required = API_Check()
+
+if Find_Required == "No" then
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = "Info";
+        Text = "Exploit is unsupported. Too run Tracer ESP";
+        Duration = math.huge;
+        Button1 = "OK"
+    })
+
+    return
+end
+
+
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local Camera = game:GetService("Workspace").CurrentCamera
+local UserInputService = game:GetService("UserInputService")
+local TestService = game:GetService("TestService")
+
+getgenv().TraceEXEcute = false
+
+
+getgenv().TeamCheck = false   -- If set to true then the script would create tracers only for the enemy team members.
+
+--[!]-- ONLY ONE OF THESE VALUES SHOULD BE SET TO TRUE TO NOT ERROR THE SCRIPT --[!]--
+
+getgenv().FromMouse = false   -- If set to true, the tracers will come from the position of your mouse curson on your screen.
+getgenv().FromCenter = true   -- If set to true, the tracers will come from the center of your screen.
+getgenv().FromBottom = false   -- If set to true, the tracers will come from the bottom of your screen.
+getgenv().TracersVisible = false   -- If set to true then the tracers will be visible and vice versa.
+
+getgenv().TracerColor = Color3.fromRGB(255,255,255)   -- The color that the tracers would appear as.
+getgenv().TracerThickness = 1   -- The thickness of the tracers.
+getgenv().TracerTransparency = 1   -- The transparency of the tracers.
+getgenv().DisableKey = Enum.KeyCode.Q   -- The key that disables / enables the tracers.
+
+
+local function CreateTracers()
+    for _, v in next, Players:GetPlayers() do
+        if v.Name ~= game.Players.LocalPlayer.Name then
+            local TracerLine = Drawing.new("Line")
+    
+            RunService.RenderStepped:Connect(function()
+                if workspace:FindFirstChild(v.Name) ~= nil and workspace[v.Name]:FindFirstChild("HumanoidRootPart") ~= nil then
+                    local HumanoidRootPart_Position, HumanoidRootPart_Size = workspace[v.Name].HumanoidRootPart.CFrame, workspace[v.Name].HumanoidRootPart.Size * 1
+                    local Vector, OnScreen = Camera:WorldToViewportPoint(HumanoidRootPart_Position * CFrame.new(0, -HumanoidRootPart_Size.Y, 0).p)
+                    
+                    TracerLine.Thickness = getgenv().TracerThickness
+                    TracerLine.Transparency = getgenv().TracerTransparency
+                    TracerLine.Color = getgenv().TracerColor
+
+                    if getgenv().FromMouse == true and getgenv().FromCenter == false and getgenv().FromBottom == false then
+                        TracerLine.From = Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y)
+                    elseif getgenv().FromMouse == false and getgenv().FromCenter == true and getgenv().FromBottom == false then
+                        TracerLine.From = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
+                    elseif getgenv().FromMouse == false and getgenv().FromCenter == false and getgenv().FromBottom == true then
+                        TracerLine.From = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y)
+                    end
+
+                    if OnScreen == true  then
+                        TracerLine.To = Vector2.new(Vector.X, Vector.Y)
+                        if getgenv().TeamCheck == true then 
+                            if Players.LocalPlayer.Team ~= v.Team then
+                                TracerLine.Visible = getgenv().TracersVisible
+                            else
+                                TracerLine.Visible = false
+                            end
+                        else
+                            TracerLine.Visible = getgenv().TracersVisible
+                        end
+                    else
+                        TracerLine.Visible = false
+                    end
+                else
+                    TracerLine.Visible = false
+                end
+            end)
+
+            Players.PlayerRemoving:Connect(function()
+                TracerLine.Visible = false
+            end)
+        end
+    end
+
+    Players.PlayerAdded:Connect(function(Player)
+        Player.CharacterAdded:Connect(function(v)
+            if v.Name ~= game.Players.LocalPlayer.Name then
+                local TracerLine = Drawing.new("Line")
+        
+                RunService.RenderStepped:Connect(function()
+                    if workspace:FindFirstChild(v.Name) ~= nil and workspace[v.Name]:FindFirstChild("HumanoidRootPart") ~= nil then
+                        local HumanoidRootPart_Position, HumanoidRootPart_Size = workspace[v.Name].HumanoidRootPart.CFrame, workspace[v.Name].HumanoidRootPart.Size * 1
+                    	local Vector, OnScreen = Camera:WorldToViewportPoint(HumanoidRootPart_Position * CFrame.new(0, -HumanoidRootPart_Size.Y, 0).p)
+                        
+                        TracerLine.Thickness =getgenv().TracerThickness
+                        TracerLine.Transparency = getgenv().TracerTransparency
+                        TracerLine.Color = getgenv().TracerColor
+
+                        if getgenv().FromMouse == true and getgenv().FromCenter == false and getgenv().FromBottom == false then
+                            TracerLine.From = Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y)
+                        elseif getgenv().FromMouse == false and getgenv().FromCenter == true and getgenv().FromBottom == false then
+                            TracerLine.From = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
+                        elseif getgenv().FromMouse == false and getgenv().FromCenter == false and getgenv().FromBottom == true then
+                            TracerLine.From = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y)
+                        end
+
+                        if OnScreen == true  then
+                            TracerLine.To = Vector2.new(Vector.X, Vector.Y)
+                            if getgenv().TeamCheck == true then 
+                                if Players.LocalPlayer.Team ~= Player.Team then
+                                    TracerLine.Visible = getgenv().TracersVisible
+                                else
+                                    TracerLine.Visible = false
+                                end
+                            else
+                                TracerLine.Visible = getgenv().TracersVisible
+                            end
+                        else
+                            TracerLine.Visible = false
+                        end
+                    else
+                        TracerLine.Visible = false
+                    end
+                end)
+
+                Players.PlayerRemoving:Connect(function()
+                    TracerLine.Visible = false
+                end)
+            end
+        end)
+    end)
+end
+CreateTracers()
