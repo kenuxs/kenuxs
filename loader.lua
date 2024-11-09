2de1f9cf0b32a410adc8a473d93283bd052d96d7
kenuxs <144792959+kenuxs@users.noreply.github.com>
2024-11-09 21:32:05 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,7 @@
 local games = {
     ["8200787440"] = "https://raw.githubusercontent.com/kenuxs/kenuxs/main/Games/Eat-Blobs-Simulator/script.lua",
+    ["13987158269"] = "https://raw.githubusercontent.com/kenuxs/kenuxs/refs/heads/main/Games/Impossible-Squid-Game-Glass-Bridge/script.lua",
+    ["16993432698"] = "https://raw.githubusercontent.com/kenuxs/kenuxs/refs/heads/main/Games/Impossible-Squid-Game-Glass-Bridge/script.lua",
 }
 
 for id, url in next, games do
